227fa74309aca630e3025cfa44f9bc53da98fdfb
Gaurav7004 <graj499@gmail.com>
2020-05-14 08:26:14 +0530

    Updates

---
+++ precache-manifest.b0710b471e0bbf49fc744b4a60378ba9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332e5f11edbeee081850570475861ff8",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "497fee1988a91e3fd908",
+    "url": "/my_portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "75c9382aad0abd92f25c",
+    "url": "/my_portfolio/static/css/main.4728b62a.chunk.css"
+  },
+  {
+    "revision": "497fee1988a91e3fd908",
+    "url": "/my_portfolio/static/js/2.a267acfb.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my_portfolio/static/js/2.a267acfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c9382aad0abd92f25c",
+    "url": "/my_portfolio/static/js/main.9d653b17.chunk.js"
+  },
+  {
+    "revision": "e7360a6458e70fd4952d",
+    "url": "/my_portfolio/static/js/runtime-main.8e144cbb.js"
+  }
+]);
